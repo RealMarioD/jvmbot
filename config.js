1c0393faf19e3593af04da9956b959ca4403f85a
RealMarioD <35196073+RealMarioD@users.noreply.github.com>
2019-06-05 16:37:54 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    owners: ["254714656336248833", "321242389106786314", "277828222677155841", "376488241580343296", "315405989098815488"], //Mario_D#7052 Cyber28#3834 MrGazdag#8214 Marton#9856 Műanyag Bogrács#8569 
-    prefix: '.',
-    invite: 'https://discord.com/invite/7EYkg3',
-    botInvite: 'https://discordapp.com/oauth2/authorize?client_id=585811477601189889&scope=bot&permissions=314432',
-    token: 'NTg1ODExNDc3NjAxMTg5ODg5.XPe6cw.CW7oN7umQ_hv9btKf8KQolfBpfo',
-    Status: 'dnd',
-}
