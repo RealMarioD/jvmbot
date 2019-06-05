d6adafc76c56e7881a36c3c79d56f5ede8e2c024
RealMarioD <mariocraft.MC2003@gmail.com>
2019-06-05 22:07:08 +0200

    Messages now get deleted

--- commands/hirfel.js
+++ commands/hirfel.js
@@ -1,4 +1,5 @@
 exports.run = (client, message, args) => {
+    message.delete()
     message.guild.members.get(message.author.id).addRole('584676489405136897')
     message.author.send(`✔️ **FELiratkoztál** a szerver hírlevelére! 📨\n\n<:vidman_logo:584352979818250257> __${message.guild.name}__`)
 }
--- commands/hirle.js
+++ commands/hirle.js
@@ -1,4 +1,5 @@
 exports.run = (client, message, args) => {
+    message.delete()
     message.guild.members.get(message.author.id).removeRole('584676489405136897')
     message.author.send(`✔️ **LEiratkoztál** a szerver hírleveléről! 📨\n\n<:vidman_logo:584352979818250257> __${message.guild.name}__`)
 }
