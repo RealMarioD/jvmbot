4c7f63b61852ca1ad82e5472ec1af30983745fdf
Pannoniae <prisznyak.zsombor@gmail.com>
2019-06-25 17:58:22 +0100

    sudoku

--- commands/eval.js
+++ commands/eval.js
@@ -7,12 +7,12 @@ const clean = text => {
 
 exports.run = (client, message, args) => {
     const config = require("../config.json");
-    if(message.author.id == config.ownerID) {
+    if(message.author.id === config.ownerID) {
 
         try {
-            const code = args.join(" ")
-            let evaled = eval(code)
-            if(typeof evaled !== "string") evaled = require("util").inspect(evaled)
+            const code = args.join(" ");
+            let evaled = eval(code);
+            if(typeof evaled !== "string") evaled = require("util").inspect(evaled);
             message.channel.send(`\`OUT:\`\n\`\`\`xl\n${clean(evaled)}\`\`\``).catch(err => message.channel.send(`\`ERROR\` \`\`\`\nToo many characters to send! (2000+)\n\`\`\``))
         } catch (err) {
             message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``).catch(err => message.channel.send(`\`ERROR\` \`\`\`\nToo many characters to send! (2000+)\n\`\`\``))
