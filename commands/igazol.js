f61da66957bdeb175563e685ec1555f1fe80b9b2
RealMarioD <mariocraft.MC2003@gmail.com>
2019-06-25 15:21:21 +0200

    Emoji fix

--- commands/igazol.js
+++ commands/igazol.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
     message.delete();
     message.guild.members.get(message.author.id).removeRole('584408550365724672');
     message.guild.members.get(message.author.id).addRole('584408550101483523');
-    message.author.send(`**Gratulálok, <@${message.author.id}>!** Mostmár láthatod a többi csatornát a szerveren és megkaptad a Tag rangot!🙂\n**Olvasd el a *<#584734791761526793>* csatornát is!**\n\nJelenleg ${message.guild.members.size} tag van a szerverben!\n\n<:vidman_logo:584352979818250257> __${message.guild.name}__`)
+    message.author.send(`**Gratulálok, <@${message.author.id}>!** Mostmár láthatod a többi csatornát a szerveren és megkaptad a Tag rangot!🙂\n**Olvasd el a *<#584734791761526793>* csatornát is!**\n\nJelenleg ${message.guild.members.size} tag van a szerverben!\n\n<:vidman_logo:588027207772012544> __${message.guild.name}__`)
 };
 
 exports.info = {
