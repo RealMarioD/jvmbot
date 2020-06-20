d4f3ec9b9e8f03a4883e2bed7984a47e0cf7bcc9
RealMarioD <mariocraft.MC2003@gmail.com>
2020-06-20 11:47:35 +0200

    fix(guildmemberadd): fixed undefined emoji in welcome msg

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,6 @@ const { getDate, getEmoji, getMention } = require('../util.js');
 module.exports = (client, member) => {
     if (member.guild.id === client.config.serverID) {
         console.log(`${member.user.tag} belépett a szerverbe. @ ${getDate()}`);
-        member.send(`${getEmoji('vidmanLogo')} __**Üdvözöllek a szerveren!**\n__Ahhoz, hogy belépj, ellenőriznünk kell, hogy nem vagy-e robot.\nAz \`${client.config.prefix}igazol\` parancs beírásával tudod magad igazolni az **${getMention(client.config.igazolID)}** csatornán.\nMindenféleképpen olvasd el az **${getMention(client.config.udvozlegyID)}** csatornát is értékes infókért!\n\nSok sikert, és jó szórakozást!\n\n\n${getEmoji(client, 'vidmanLogo')} __${member.guild.name}__`);
+        member.send(`${getEmoji('vidmanLogo')} __**Üdvözöllek a szerveren!**\n__Ahhoz, hogy belépj, ellenőriznünk kell, hogy nem vagy-e robot.\nAz \`${client.config.prefix}igazol\` parancs beírásával tudod magad igazolni az **${getMention(client.config.igazolID)}** csatornán.\nMindenféleképpen olvasd el az **${getMention(client.config.udvozlegyID)}** csatornát is értékes infókért!\n\nSok sikert, és jó szórakozást!\n\n\n${getEmoji('vidmanLogo')} __${member.guild.name}__`);
     }
 };
